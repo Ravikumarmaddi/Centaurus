e291c4824ef108350f4f1ab86a4c0a24455a46ed
Vlad <v.socolov@yahoo.com>
2013-09-28 23:49:56 +0300

    minor changes in Scanner logic

--- centaurus-core/src/main/java/org/centaurus/Scanner.java
+++ centaurus-core/src/main/java/org/centaurus/Scanner.java
@@ -55,30 +55,32 @@ public class Scanner extends ScannerTemplate{
 			}	
 		}
 		
-		URL[] urls = (URL[])urlPaths.toArray(new URL[1]);
-		for (URL url : urls) {
-			try {
-				db.scanArchives(url);
-				if(db.getAnnotationIndex().get(Document.class.getName()) != null){
-					String[] entityClasses = (String[]) db.getAnnotationIndex().get(Document.class.getName()).toArray(new String[0]);
-					for(int i = 0; i < entityClasses.length; i++) {
-						String className = null;
-						try{
-							className = entityClasses[i];
-							if(!CentaurusConfig.getInstance().getMappedClasses().contains(className)){
-								Class<?> clazz = Class.forName(className);
-								CentaurusConfig.getInstance().getMappedClasses().add(clazz);
-								log.info(String.format("Scanned %s class.", className));
+		URL[] urls = urlPaths.isEmpty() ? null : (URL[])urlPaths.toArray(new URL[1]);
+		if(urls != null){
+			for (URL url : urls) {
+				try {
+					db.scanArchives(url);
+					if(db.getAnnotationIndex().get(Document.class.getName()) != null){
+						String[] entityClasses = (String[]) db.getAnnotationIndex().get(Document.class.getName()).toArray(new String[0]);
+						for(int i = 0; i < entityClasses.length; i++) {
+							String className = null;
+							try{
+								className = entityClasses[i];
+								if(!CentaurusConfig.getInstance().getMappedClasses().contains(className)){
+									Class<?> clazz = Class.forName(className);
+									CentaurusConfig.getInstance().getMappedClasses().add(clazz);
+									log.info(String.format("Scanned %s class.", className));
+								}
+							} catch (ClassNotFoundException e) {
+								log.error(String.format("Cannot extract class type of name: %s", className));
 							}
-						} catch (ClassNotFoundException e) {
-							log.error(String.format("Cannot extract class type of name: %s", className));
 						}
 					}
-				}
-				
-			} catch (IOException e) {
-				log.error(String.format("Cannot scan package: %s", url.toString()));
-			} 
+					
+				} catch (IOException e) {
+					log.error(String.format("Cannot scan package: %s", url.toString()));
+				} 
+			}	
 		}
 	}
 	
